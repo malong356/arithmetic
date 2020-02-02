64d3b44c53059d8ae3d779c57ca0f89e6d3e97cf
malong <malong356@163.com>
2020-02-02 21:22:19 +0800

    1

---
+++ src/chapter1/_010_最大值减去最小值小于或等于num的子数组数量.java
@@ -0,0 +1,52 @@
+package chapter1;
+
+import java.util.LinkedList;
+
+/**
+ * 最大值减去最小值小于或等于num的子数组数量
+ * 时间复杂度为O(N)
+ * Created by malong on 2020-02-02.
+ */
+public class _010_最大值减去最小值小于或等于num的子数组数量 {
+    /**
+     * 双端队列实现
+     * qmin qmax存储数组位子信息
+     */
+    public static int getNum(int[] arr, int num){
+        if(arr == null || arr.length == 0 || num < 0) {
+            return 0;
+        }
+        LinkedList<Integer> qmin = new LinkedList<>();
+        LinkedList<Integer> qmax = new LinkedList<>();
+        int i = 0;
+        int j = 0;
+        int res = 0;
+        while (i < arr.length){
+            while (j < arr.length){
+                if (qmin.isEmpty() || qmin.peekLast() != j){
+                    while (!qmin.isEmpty() && arr[qmin.peekLast()] >= arr[j]) {
+                        qmin.pollLast();
+                    }
+                    qmin.addLast(j);
+                    while (!qmax.isEmpty() || arr[qmin.peekLast()] <= arr[j]){
+                        qmax.pollLast();
+                    }
+                    qmax.addLast(j);
+                }
+                if(arr[qmax.getFirst()] - arr[qmin.getFirst()] > num){
+                    break;
+                }
+                j++;
+            }
+            res += j - i;
+            if(qmin.peekFirst() == i){
+                qmin.pollFirst();
+            }
+            if(qmax.peekFirst() == i){
+                qmax.pollFirst();
+            }
+            i++;
+        }
+        return res;
+    }
+}
---
+++ src/chapter1/_011_可见的山峰对数量.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+/**
+ * 可见的山峰对数量
+ * Created by malong on 2020-02-02.
+ */
+public class _011_可见的山峰对数量 {
+
+
+}
